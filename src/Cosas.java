9418ca2bc2648ea4ed6bedf223be43bf2e751999
jballes <jballes@jballes>
2020-07-23 14:48:56 +0200

    Creados métodos para limpieza de código

--- src/Cosas.java
+++ src/Cosas.java
@@ -1,6 +1,11 @@
 import java.lang.Math;
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Jorge Ballesteros
+ *
+ */
 public class Cosas {
 
 	static double x;
@@ -12,13 +17,13 @@ public class Cosas {
 
 		recogidaDeDatos();
 
-		System.out.print("Ángulo formado por el vector de O al punto ingresado y el plano OYZ:  ");
+		System.out.print("Ãngulo formado por el vector de O al punto ingresado y el plano OXZ:  ");
 		System.out.println(calcularAngulo(1));
 
-		System.out.print("Ángulo formado por el vector de O al punto ingresado y el plano OXY:  ");
+		System.out.print("Ãngulo formado por el vector de O al punto ingresado y el plano OXY:  ");
 		System.out.println(calcularAngulo(2));
 
-		System.out.print("Ángulo formado por el vector de O al punto ingresado y el plano OXZ:  ");
+		System.out.print("Ãngulo formado por el vector de O al punto ingresado y el plano OYZ:  ");
 		System.out.println(calcularAngulo(3));
 	}
 
@@ -30,92 +35,26 @@ public class Cosas {
 		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Ingrese la primera coordenada del 0 al 10");
-
-		boolean repetir = false;
-		do {
-			try {
-				x = Double.parseDouble(sc.next());
-				repetir = false;
-
-			} catch (NumberFormatException e) {
-				System.err.println("Ingrese un caracter válido");
-				repetir = true;
-				x = 0;
-			}
-			try {
-				while (x > 10 || x < 0) {
-					System.err.println("Ingrese un número dentro del rango (0-10)");
-					x = Double.parseDouble(sc.next());
-				}
-			} catch (NumberFormatException e) {
-				System.err.println("Ingrese un caracter válido");
-				repetir = true;
-				x = 0;
-			}
-		} while (repetir);
+		obtenerCoordenadaX(sc);
 
 		System.out.println("Ingrese la segunda coordenada del 0 al 10");
-
-		do {
-			try {
-				y = Double.parseDouble(sc.next());
-				repetir = false;
-
-			} catch (NumberFormatException e) {
-				System.err.println("Ingrese un caracter válido");
-				repetir = true;
-				y = 0;
-
-			}
-			try {
-				while (y > 10 || y < 0) {
-					System.err.println("Ingrese un número dentro del rango (0-10)");
-					y = Double.parseDouble(sc.next());
-				}
-			} catch (NumberFormatException e) {
-				System.err.println("Ingrese un caracter válido");
-				repetir = true;
-				y = 0;
-			}
-		} while (repetir);
+		obtenerCoordenadaY(sc);
 
 		System.out.println("Ingrese la tercera coordenada del 0 al 10");
+		obtenerCoordenadaZ(sc);
 
-		do {
-			try {
-				z = Double.parseDouble(sc.next());
-				repetir = false;
-
-			} catch (NumberFormatException e) {
-				System.err.println("Ingrese un caracter válido");
-				repetir = true;
-				z = 0;
-
-			}
-			try {
-				while (z > 10 || z < 0) {
-					System.err.println("Ingrese un número dentro del rango (0-10)");
-					z = Double.parseDouble(sc.next());
-				}
-			} catch (NumberFormatException e) {
-				System.err.println("Ingrese un caracter válido");
-				repetir = true;
-				z = 0;
-			}
-		} while (repetir);
-
-		System.out.println("Ingrese el factor de multiplicación cualquiera");
+		System.out.println("Ingrese el factor de multiplicaciï¿½n cualquiera");
 		double factor;
 
 		try {
 			factor = Double.parseDouble(sc.next());
 
 		} catch (NumberFormatException e) {
-			System.err.println("Ingrese un caracter válido");
+			System.err.println("Ingrese un caracter vï¿½lido");
 			factor = Double.parseDouble(sc.next());
 
 		}
-		vector1 = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2))*factor;
+		vector1 = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) * factor;
 
 		System.out.println(vector1);
 
@@ -123,34 +62,133 @@ public class Cosas {
 	}
 
 	/**
-	 * Devuelve el ángulo calculado del vector con los 3 planos principales
-	 * @param a el caso de con qué plano se quiere calcular (1 OYZ, 2 OXY, 3OXZ)
-	 * @return el ángulo correspondiente
+	 * Pide al usuario la tercera coordenada del punto en cuestiÃ³n
+	 * @param sc
+	 */
+	private static void obtenerCoordenadaZ(Scanner sc) {
+		boolean repetir;
+		do {
+			try {
+				z = Double.parseDouble(sc.next());
+				repetir = false;
+
+			} catch (NumberFormatException e) {
+				System.err.println("Ingrese un caracter vï¿½lido");
+				repetir = true;
+				z = 0;
+
+			}
+			try {
+				while (z > 10 || z < 0) {
+					System.err.println("Ingrese un nï¿½mero dentro del rango (0-10)");
+					z = Double.parseDouble(sc.next());
+				}
+			} catch (NumberFormatException e) {
+				System.err.println("Ingrese un caracter vï¿½lido");
+				repetir = true;
+				z = 0;
+			}
+		} while (repetir);
+	}
+
+	/**
+	 * Pide al usuario la segunda coordenada del punto en cuestiÃ³n
+	 * 
+	 * @param sc
+	 */
+	private static void obtenerCoordenadaY(Scanner sc) {
+		boolean repetir;
+		do {
+			try {
+				y = Double.parseDouble(sc.next());
+				repetir = false;
+
+			} catch (NumberFormatException e) {
+				System.err.println("Ingrese un caracter vï¿½lido");
+				repetir = true;
+				y = 0;
+
+			}
+			try {
+				while (y > 10 || y < 0) {
+					System.err.println("Ingrese un nï¿½mero dentro del rango (0-10)");
+					y = Double.parseDouble(sc.next());
+				}
+			} catch (NumberFormatException e) {
+				System.err.println("Ingrese un caracter vï¿½lido");
+				repetir = true;
+				y = 0;
+			}
+		} while (repetir);
+	}
+
+	/**
+	 * Pide al usuario la primera coordenada del punto en cuestiÃ³n
+	 * 
+	 * @param sc
+	 */
+	private static void obtenerCoordenadaX(Scanner sc) {
+
+		boolean repetir;
+
+		do {
+
+			// El try con el parseDouble prueba si lo que se ha introducido es un nÃºmero,
+			// en caso contrario se pasa al catch debido a que se ha introducido un caracter
+			// diferente
+
+			try {
+				x = Double.parseDouble(sc.next());
+				repetir = false;
+
+			} catch (NumberFormatException e) {
+				System.err.println("Ingrese un caracter vï¿½lido");
+				repetir = true;
+				x = 0;
+			}
+			try {
+				while (x > 10 || x < 0) {
+					System.err.println("Ingrese un nï¿½mero dentro del rango (0-10)");
+					x = Double.parseDouble(sc.next());
+				}
+			} catch (NumberFormatException e) {
+				System.err.println("Ingrese un caracter vï¿½lido");
+				repetir = true;
+				x = 0;
+			}
+		} while (repetir);
+	}
+
+	/**
+	 * Devuelve el ï¿½ngulo calculado del vector con los 3 planos principales
+	 * 
+	 * @param a el caso de con quï¿½ plano se quiere calcular (1 OYZ, 2 OXY, 3OXZ)
+	 * @return el ï¿½ngulo correspondiente
 	 */
 	public static double calcularAngulo(int a) {
 
 		switch (a) {
 
 		case 1:
-			//Caso de prueba x=3, y=2, z=1
-			//Resultados esperados: alpha=15.5º beta=32.31º gamma=53.3º
-			//GeoGebra: https://www.geogebra.org/3d/zfrn35mh
-			
-			//Plano base según maqueta
+			// Caso de prueba x=3, y=2, z=1
+			// Resultados esperados: alpha=15.5ï¿½ beta=32.31ï¿½ gamma=53.3ï¿½
+			// GeoGebra: https://www.geogebra.org/3d/zfrn35mh
+
+			// Plano base segÃºn maqueta
 			double anguloOXZ = 0;
-			anguloOXZ = 1/(Math.sin(y / vector1));
+			anguloOXZ = y / vector1;
 			return anguloOXZ;
 
 		case 2:
-			//Plano derecho según maqueta
+			// Plano derecho segÃºn maqueta
 			double anguloOXY = 0;
-			anguloOXY = Math.sin(z / vector1);
+			anguloOXY = Math.asin(z / vector1);
 			return anguloOXY;
 
 		case 3:
-			//Plano izquierdo según maqueta
+			// Plano izquierdo segÃºn maqueta
 			double anguloOYZ = 0;
-			anguloOYZ = Math.sin(x / vector1);
+			anguloOYZ = Math.asin(x / vector1);
 			return anguloOYZ;
 
 		default:
